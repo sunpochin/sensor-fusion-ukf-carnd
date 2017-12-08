7c57339709d59c65e77c9602207c415225838dd8
Sun Pochin <sunpochin@gmail.com>
2017-12-08 15:17:34 +0800

    Coding to predict mean and covariance, now the green dots are running but wrongly moving on y-axis.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -78,6 +78,11 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
    ****************************************************************************/
 
   if (!is_initialized_) {
+    // important! of   Prediction(delta_t); will fail due to too large values.
+
+
+    time_us_ = meas_package.timestamp_ ;
+
     if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
       // Convert radar from polar to cartesian coordinates and initialize state.
       cout << "RADAR : measurement_pack.raw_measurements_ : " << meas_package.raw_measurements_ << endl;
@@ -145,10 +150,20 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+
+  // important: must change delta_t from us to seconds, or the angle normalization in step 4 would be too large to perform.
+  delta_t = delta_t / 1000000;
   cout << "Prediction, delta_t : " << delta_t << endl;
-  /// generate sigma points. from: Generating Sigma Points Assignment 1
+
+  /// step 1: generate sigma points. from: Generating Sigma Points Assignment 1
+  /// step 2: sigma point augmentation. from "UKF Augmentation Assignment 1"
+  /// step 3: Predict sigma points. codes from "Sigma Point Prediction Assignment 1"
+  /// step 4: Predict Mean And Covariance . from "Predicted Mean and Covariance Assignment 1"
+
+
+  /// step 1: generate sigma points. from: Generating Sigma Points Assignment 1
   //create sigma point matrix
-  // todo: move these to init/Constructor?
+  // todo: move these initilization to Constructor?
   //set state dimension
   n_x_ = 5;
   lambda_ = 3 - n_x_ ;
@@ -169,9 +184,10 @@ void UKF::Prediction(double delta_t) {
   }
 
 
-  /// UKF Augmentation Assignment 1
+  /// step 2: sigma point augmentation, from "UKF Augmentation Assignment 1"
   //create augmented mean state
   //set augmented dimension
+  // todo: move these initilization to Constructor?
   n_aug_ = 7;
   //create augmented mean vector
   VectorXd x_aug = VectorXd(n_aug_);
@@ -185,18 +201,15 @@ void UKF::Prediction(double delta_t) {
   x_aug(6) = 0;
 //  std::cout << "x_aug = " << std::endl << x_aug << std::endl;
 
-//return;
   //create augmented covariance matrix
   P_aug.fill(0.0);
   P_aug.topLeftCorner(5,5) = P_;
   P_aug(5,5) = std_a_ * std_a_ ;
   P_aug(6,6) = std_yawdd_ * std_yawdd_;
 //  std::cout << "P_aug = " << std::endl << P_aug << std::endl;
-//return;
   //create square root matrix
   MatrixXd L = P_aug.llt().matrixL();
 
-// return;
   //create augmented sigma points
   Xsig_aug.col(0)  = x_aug;
   for (int i = 0; i < n_aug_; i++)
@@ -205,6 +218,101 @@ void UKF::Prediction(double delta_t) {
     Xsig_aug.col(i+1 + n_aug_) = x_aug - sqrt(lambda_ + n_aug_) * L.col(i);
   }
 
+
+  /// step 3: Predict sigma points. codes from "Sigma Point Prediction Assignment 1"
+  //create matrix with predicted sigma points as columns
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+  // Xsig_pred.fill(0.0);
+  std::cout << "Xsig_pred_ = " << std::endl << Xsig_pred_ << std::endl;
+  for (int i = 0; i < 2 * n_aug_ + 1; i++)
+  {
+
+    double p_x = Xsig_aug(0, i);
+    double p_y = Xsig_aug(1, i);
+
+    double v = Xsig_aug(2, i);
+    double yaw = Xsig_aug(3, i);
+    double yawd = Xsig_aug(4, i);
+
+    double nu_a = Xsig_aug(5, i);
+    double nu_yawdd = Xsig_aug(6, i);
+
+    //predicted state values
+    double px_p, py_p;
+    //avoid division by zero
+    if ( fabs(yawd) > 0.001 ) {
+        px_p = p_x + v/yawd * ( sin(yaw + yawd * delta_t ) - sin(yaw)  );
+        py_p = p_y + v/yawd * ( -cos(yaw + yawd * delta_t ) + cos(yaw)  );
+//        py_p = p_y + v/yawd * ( cos(yaw) - cos(yaw+yawd*delta_t) );
+    } else {
+        px_p = p_x + v*delta_t*cos(yaw);
+        py_p = p_y + v*delta_t*sin(yaw);
+    }
+    //add noise
+    px_p = px_p + 0.5*nu_a*delta_t*delta_t * cos(yaw);
+    py_p = py_p + 0.5*nu_a*delta_t*delta_t * sin(yaw);
+
+    //write predicted sigma points into right column
+    Xsig_pred_(0,i) = px_p;
+    Xsig_pred_(1,i) = py_p;
+
+    double v_p = v;
+    v_p = v_p + nu_a*delta_t;
+    Xsig_pred_(2,i) = v_p;
+
+    double yaw_p = yaw + yawd*delta_t;
+    yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
+    Xsig_pred_(3,i) = yaw_p;
+
+    double yawd_p = yawd;
+    yawd_p = yawd_p + nu_yawdd*delta_t;
+    Xsig_pred_(4,i) = yawd_p;
+  }
+  std::cout << "Xsig_pred_ : " << std::endl << Xsig_pred_ << std::endl ;
+
+// return;
+
+  /// step 4: Predict Mean And Covariance . from "Predicted Mean and Covariance Assignment 1"
+  weights_ = VectorXd(2 * n_aug_ + 1);
+  weights_(0) = lambda_ / (lambda_ + n_aug_);
+  for (int it = 1; it < 2 * n_aug_ + 1; it++) {
+      weights_(it) = 1 / (2 * (lambda_ + n_aug_) );
+      // double weight = 0.5 / (lambda + n_aug);
+      // weights(it) = weight;
+  }
+  std::cout << "weights : " << weights_ << std::endl ;
+
+  //predict state mean
+  x_.fill(0.0);
+  for (int it = 0; it < 2 * n_aug_ + 1; it++) {
+      VectorXd tempmul = weights_(it) * Xsig_pred_.col(it) ;
+      std::cout << "weights : " << weights_(it) << std::endl ;
+      std::cout << "Xsig_pred.col(it)  : " << Xsig_pred_.col(it) << std::endl ;
+      std::cout << "tempmul  : " << tempmul << std::endl ;
+      x_ = x_ + weights_(it) * Xsig_pred_.col(it) ;
+  }
+  std::cout << "x_ : " << std::endl << x_ << std::endl ;
+  // predict state covariance matrix
+  P_.fill(0.0);
+  for (int it = 0; it < 2 * n_aug_ + 1; it++) {
+      // state difference
+      VectorXd x_diff = Xsig_pred_.col(it) - x_;
+      //angle normalization
+      while (x_diff(3) > M_PI) {
+          x_diff(3) -= 2. * M_PI;
+          // std::cout << "x_diff(3) > : " << x_diff(3) << std::endl ;
+          // std::cout << "M_PI : " << M_PI << std::endl ;
+      }
+      while (x_diff(3) < -M_PI) {
+          x_diff(3) += 2.*M_PI;
+          // std::cout << "x_diff(3) <- : " << x_diff(3) << std::endl ;
+          // std::cout << "M_PI : " << M_PI << std::endl ;
+      }
+      P_ = P_ + weights_(it) * x_diff * x_diff.transpose() ;
+  }
+
+  /// step 5:
+  std::cout << "step 5 : " << std::endl ;
 
 
 }
